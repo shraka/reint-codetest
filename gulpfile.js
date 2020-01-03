594d51ddb946054215d402721e365770dbec4c79
Nim <nimrod.evans@gmail.com>
2020-01-03 17:15:38 +1100

    Readme + Deployment
    - Changed deployment method to add build mode
    - Added readme

--- gulpfile.js
+++ gulpfile.js
@@ -4,56 +4,66 @@ const 	gulp = require("gulp"),
 	    autoprefixer = require("autoprefixer"),
 	    cssnano = require("cssnano"),
 	    sourcemaps = require("gulp-sourcemaps"),
-        uglify = require('gulp-uglify');
+        uglify = require('gulp-uglify'),
+        gulpif = require('gulp-if'),
+        readlineSync = require('readline-sync');
 
 const paths = {
     styles: {
         src: 'src/_sass/*.scss',
-        dest: 'dist/css/'
+        dest: 'build/css/',
+        release: 'dist/css/'
     },
     scripts: {
         src: ['src/_scripts/main.js','src/_scripts/plugins.js'],
-        dest: 'dist/js/'
+        dest: 'build/js/',
+        release: 'dist/js/'
     },
     files: {
         src: ['src/**/*.*', '!src/_*/**/*'],
-        dest: 'dist/'
+        dest: 'build/',
+        release: 'dist/'
     },
     sourcemaps: '../../maps'
 };
 
-function moveFiles() {
+function moveFiles(release = false) {
+    const dest = (release == true) ? paths.files.release: paths.files.dest;
     return (
         gulp.src(paths.files.src)
-            .pipe(gulp.dest(paths.files.dest))
+            .pipe(gulp.dest(dest))
     );
 }
 exports.moveFiles = moveFiles;
 
-function style() {
+function style(release = false) {
+    const dest = (release == true) ? paths.styles.release: paths.styles.dest;
     return (
         gulp.src(paths.styles.src)
  			.pipe(sourcemaps.init())
             .pipe(sass())
             	.on("error", sass.logError)
-            // .pipe(postcss([autoprefixer(), cssnano()]))
-            .pipe(sourcemaps.write(paths.sourcemaps))
-            .pipe(gulp.dest(paths.styles.dest))
+            .pipe(postcss([autoprefixer(), cssnano()]))
+            .pipe(gulpif(release!=release,sourcemaps.write(paths.sourcemaps)))
+            .pipe(gulp.dest(dest))
     );
 }
 exports.style = style;
 
-function script() {
+function script(release = false) {
+    const dest = (release == true) ? paths.scripts.release: paths.scripts.dest;
     return (
         gulp.src(paths.scripts.src)
             .pipe(sourcemaps.init())
             .pipe(uglify())
-            .pipe(sourcemaps.write(paths.sourcemaps))
-            .pipe(gulp.dest(paths.scripts.dest))
+            .pipe(gulpif(release!=release,sourcemaps.write(paths.sourcemaps)))
+            .pipe(gulp.dest(dest))
 
     );
 }
 exports.script = script;
+
+// Standard functions
 
 function watch(){
     gulp.watch(paths.files.src, moveFiles);
@@ -69,3 +79,24 @@ function build(cb){
     cb();
 }
 exports.build = build
+
+// Release functions
+function confirmRelease(cb) {
+    if (readlineSync.keyInYN('Are you sure you want to release?')) {
+      return cb();
+    }
+    console.log('Ok, not releasing.');
+    process.exit(1);
+}
+
+function release(cb) {
+    console.log('////////////////////');
+    console.log('// RELEASING CODE //');
+    console.log('////////////////////');
+    moveFiles(true);
+    script(true);
+    style(true);
+    cb();
+}
+
+exports.release = gulp.series(confirmRelease, release);
---
+++ readme.md
@@ -0,0 +1,45 @@
+# Reinteractive Code Test by Nimrod Evans
+
+Hi Joelle. Here's my somewhat over the top code challenge.
+
+## Getting Started
+Just download the repo to take a look. I've included the dist so you don't have to compile anything.
+
+```bash
+git clone https://github.com/shraka/reint-codetest.git
+```
+
+### Compiling
+You should need to do this, but if you want to compile the project you'll need NPM.
+Note that I haven't included a server, or any tests. 
+
+### Installing
+
+```bash
+npm install
+```
+
+### Compiling
+This outputs to the build/ folder
+
+```bash
+gulp build
+```
+
+### Watch function
+Automatically outputs build/ folder
+
+```bash
+gulp watch
+```
+
+### Release function
+Outputs to the dist/ folder for distribution
+
+```bash
+gulp release
+```
+
+When prompted, press 'Y'
+
+That's all.
